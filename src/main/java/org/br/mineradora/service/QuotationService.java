4e9d4d4cc1e5eb7bdae710de78e3516f24f366f1
GabrielDCastro <gabrielrangel12@gmail.com>
2023-08-26 16:41:58 -0300

    Logic to update price

---
+++ src/main/java/org/br/mineradora/service/QuotationService.java
@@ -0,0 +1,75 @@
+package org.br.mineradora.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.br.mineradora.client.CurrencyPriceClient;
+import org.br.mineradora.dto.CurrencyPriceDTO;
+import org.br.mineradora.dto.QuotationDTO;
+import org.br.mineradora.entity.QuotationEntity;
+import org.br.mineradora.message.KafkaEvents;
+import org.br.mineradora.repository.QuotationRepository;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@ApplicationScoped
+public class QuotationService {
+
+    @RestClient
+    @Inject
+    CurrencyPriceClient currencyPriceClient;
+
+    @Inject
+    QuotationRepository quotationRepository;
+
+    @Inject
+    KafkaEvents kafkaEvents;
+
+    public void getCurrencyPrice(){
+
+        CurrencyPriceDTO currencyPriceInfo = currencyPriceClient.getPriceByPair("USD-BRL");
+
+        if(updateCurrencyInfoPrice(currencyPriceInfo)){
+            kafkaEvents.sendNewKafkaEvent(QuotationDTO
+                    .builder()
+                    .currencyPrice(new BigDecimal(currencyPriceInfo.getUSDBRL().getBid()))
+                    .date(new Date())
+                    .build());
+        }
+    }
+
+    private boolean updateCurrencyInfoPrice(CurrencyPriceDTO currencyPriceInfo) {
+        BigDecimal currencyPrice = new BigDecimal(currencyPriceInfo.getUSDBRL().getBid());
+        boolean updatePrince =false;
+
+        List<QuotationEntity> quotationList = quotationRepository.findAll().list();
+
+        if(quotationList.isEmpty()){
+            saveQuotation(currencyPriceInfo);
+            updatePrince = true;
+        }
+        else {
+            QuotationEntity lastDollarPrice = quotationList.get(quotationList.size() -1);
+
+            if (currencyPrice.floatValue() > lastDollarPrice.getCurrencyPrice().floatValue()){
+                updatePrince = true;
+                saveQuotation(currencyPriceInfo);
+            }
+        }
+        return updatePrince;
+    }
+
+    private void saveQuotation(CurrencyPriceDTO currencyInfo){
+        QuotationEntity quotation = new QuotationEntity();
+
+        quotation.setDate(new Date());
+        quotation.setCurrencyPrice(new BigDecimal(currencyInfo.getUSDBRL().getBid()));
+        quotation.setPctChange(currencyInfo.getUSDBRL().getPctChange());
+        quotation.setPair("USD-BRL");
+        quotationRepository.persist(quotation);
+    }
+
+}
